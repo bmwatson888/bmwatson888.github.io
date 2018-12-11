f92432d071212c81fa7b011347b542d6b6da8098
bmwatson888 <bmwatson@csu.fullerton.edu>
2018-12-11 11:50:09 -0800

    new post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Living in the asteroid belt isn’t exactly a walk in the park. Neither is being a miner. Put them together, and you’ve got the most dangerous job this side of Jupiter’s orbit. Can’t really complain about the pay though; I’ll make more living and working here in two years than...","categories": ["Short-Stories"],
         "tags": [],
         "url": "http://localhost:4000/short-stories/2018/12/10/juniper.html",
+        "teaser":null},{
+        "title": "So, what's this site all about?",
+        "excerpt":"I’ve been an IT guy for two decades, and have two degrees in computer science related fields. During my time working as an “IT guy” and pursuing my education, I’ve always taken an interest in reading and telling stories. I’m a huge board gamer, and the thing I love most...","categories": ["About"],
+        "tags": [],
+        "url": "http://localhost:4000/about/2018/12/11/whatsthissiteallabout.html",
         "teaser":null}]
