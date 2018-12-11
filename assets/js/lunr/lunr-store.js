3e5ecfb5a1bf95132e7e76f100048867c0ca88f7
bmwatson888 <bmwatson@csu.fullerton.edu>
2018-12-10 20:22:58 -0800

    completed main theme updates

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,33 +4,8 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/2018/12/10/welcome-to-jekyll.html",
         "teaser":null},{
-        "title": "Testng!",
-        "excerpt":"test  ","categories": ["jekyll","update"],
+        "title": "Juniper",
+        "excerpt":"Living in the asteroid belt isn’t exactly a walk in the park. Neither is being a miner. Put them together, and you’ve got the most dangerous job this side of Jupiter’s orbit. Can’t really complain about the pay though; I’ll make more living and working here in two years than...","categories": ["Short-Stories"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/testing-Copy-(2).html",
-        "teaser":null},{
-        "title": "Testng!",
-        "excerpt":"test  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/testing-Copy-(3).html",
-        "teaser":null},{
-        "title": "Testng!",
-        "excerpt":"test  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/testing-Copy-(4).html",
-        "teaser":null},{
-        "title": "Testng!",
-        "excerpt":"test  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/testing-Copy-(5).html",
-        "teaser":null},{
-        "title": "Testng!",
-        "excerpt":"test  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/testing-Copy.html",
-        "teaser":null},{
-        "title": "Testng!",
-        "excerpt":"test  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/testing.html",
+        "url": "http://localhost:4000/short-stories/2018/12/10/testing.html",
         "teaser":null}]
