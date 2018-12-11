5026be4a6ed141a9888f3cad76f9ad313e426b7f
bmwatson888 <bmwatson@csu.fullerton.edu>
2018-12-11 10:43:14 -0800

    minor theme changes and added about page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,8 +1,8 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "How this site was made...",
+        "excerpt":"I’m not a big fan of hosting providers, so this site is run on static HTML and CSS pages, hosted within GitHub for free. However, the pages of this site are all dynamically generated using Jekyll, which is a Ruby-based application that converts flat template files into HTML pages. The...","categories": ["About"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2018/12/10/welcome-to-jekyll.html",
+        "url": "http://localhost:4000/about/2018/12/10/howthissitewasmade.html",
         "teaser":null},{
         "title": "Juniper",
         "excerpt":"Living in the asteroid belt isn’t exactly a walk in the park. Neither is being a miner. Put them together, and you’ve got the most dangerous job this side of Jupiter’s orbit. Can’t really complain about the pay though; I’ll make more living and working here in two years than...","categories": ["Short-Stories"],
